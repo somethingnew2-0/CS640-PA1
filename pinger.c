30cbaac252a5452f6a6c579402f2a439a56e3f31
Cory Groom <cpgroom@wisc.edu>
2013-10-17 20:57:16 -0500

    sending and receiving a single message with NULL. start receiver first for it to work.

--- pinger.c
+++ pinger.c
@@ -1,47 +1,68 @@
 /* The pinger sends a specified series of packets via UDP to the reflector */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <ctype.h>
 #include "host.h"
 #include "utility.h"
+#include "udp.h"
 
 int main(int argc, char * argv[]) {
-  char* pingerPort = NULL;
+  char* pingerPortStr = NULL;
   char* hostname = NULL;
-  char* reflectorPort = NULL;
+  char* reflectorPortStr = NULL;
   char* numPacketsStr = NULL;
-  int numPackets = -1;
+  int pingerPort;
+  int reflectorPort;
+  int numPackets;
 
+  struct sockaddr_in* reflectorAddr = 
+    (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
+  int fd;
   int c;
 
   while((c = getopt(argc, argv, "p:s:r:n:")) != -1) {
     switch (c) {
     case 'p':
-      pingerPort = optarg;
+      pingerPortStr = optarg;
       break;
     case 's':
       hostname = optarg;
       break;
     case 'r':
-      reflectorPort = optarg;
+      reflectorPortStr = optarg;
       break;
     case 'n':
       numPacketsStr = optarg;
       break;
-    case '?':
+    default:
       printUsage();
       return 1;
-    default:
-      exit(EXIT_FAILURE);
     }
   }
   
-  checkFlagPresent('p', pingerPort);
+  checkFlagPresent('p', pingerPortStr);
   checkFlagPresent('s', hostname);
-  checkFlagPresent('r', reflectorPort);
+  checkFlagPresent('r', reflectorPortStr);
   checkFlagPresent('n', numPacketsStr);
+  pingerPort = atoi(pingerPortStr);
+  reflectorPort = atoi(reflectorPortStr);
+  numPackets = atoi(numPacketsStr);
+
+  if((fd = UDP_Open(pingerPort)) <= 0) {
+    printf("UDP_Open error\n");
+    return 1;
+  }
+  printf("Open socket\n");
+  if(UDP_FillSockAddr(reflectorAddr, hostname, reflectorPort) != 0) {
+    printf("UDP_Fill error\n");
+    return 1;
+  }
+  if(UDP_Write(fd, reflectorAddr, NULL, 0) < 0) {
+    printf("Send error\n");
+    return 1;
+  }
+  printf("Packet sent\n");
+  
+  UDP_Close(fd);
 
   return 0;
 }
--- reflector.c
+++ reflector.c
@@ -1,37 +1,40 @@
 /* The reflector receives packets from the pinger and either returns 
  * them via UDP to the pinger or drops them.
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <unistd.h>
 #include "host.h"
 #include "utility.h"
+#include "udp.h"
 
 bool dropPacket(int lossProb) {
   return (rand() % 100 + 1) <= lossProb;
 }
 
 int main(int argc, char *argv[]) {
-  char* reflectorPort = NULL;
+  char* reflectorPortStr = NULL;
   char* hostname = NULL;
-  char* pingerPort = NULL;
+  char* pingerPortStr = NULL;
   char* delayStr = NULL;
   char* lossProbStr = NULL;
+  int reflectorPort;
+  int pingerPort;
   int delay;
   int lossProb;
+
+  struct sockaddr_in* pingerAddr = 
+    (struct sockaddr_in*)malloc(sizeof(struct sockaddr_in));
+  int fd;
   int c;
 
   while((c = getopt(argc, argv, "p:s:g:d:l:")) != -1) {
     switch (c) {
     case 'p':
-      reflectorPort = optarg;
+      reflectorPortStr = optarg;
       break;
     case 's':
       hostname = optarg;
       break;
     case 'g':
-      pingerPort = optarg;
+      pingerPortStr = optarg;
       break;
     case 'd':
       delayStr = optarg;
@@ -47,16 +50,33 @@ int main(int argc, char *argv[]) {
     }
   }
   
-  checkFlagPresent('p', reflectorPort);
+  checkFlagPresent('p', reflectorPortStr);
   checkFlagPresent('s', hostname);
-  checkFlagPresent('g', pingerPort);
+  checkFlagPresent('g', pingerPortStr);
+  reflectorPort = atoi(reflectorPortStr);
+  pingerPort = atoi(pingerPortStr);
   delay = (delayStr == NULL) ? 0 : atoi(delayStr);
   lossProb = (lossProbStr == NULL) ? 0 : atoi(lossProbStr);
 
   /* initialize random seed: */
-  srand(time(NULL));
+  /*srand(time(NULL));*/
 
-
+  if((fd = UDP_Open(reflectorPort)) <= 0) {
+    printf("UDP_Open error\n");
+    return 1;
+  }
+  printf("Open socket\n");
+  if(UDP_FillSockAddr(pingerAddr, hostname, pingerPort) != 0) {
+    printf("UDP_Fill error\n");
+    return 1;
+  }
+  if(UDP_Read(fd, pingerAddr, NULL, 0) < 0) {
+    printf("Read error\n");
+    return 1;
+  }
+  printf("Packet received\n");
+  
+  UDP_Close(fd);
   return 0;
 }
 
