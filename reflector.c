c7e5baa60d285926f6f379f3b4d58a48f3b199db
Peter Collins <pmcollins2@wisc.edu>
2013-10-19 14:59:59 -0500

    Pushing code, taking names

--- reflector.c
+++ reflector.c
@@ -11,19 +11,21 @@ bool dropPacket(int lossProb) {
   return (rand() % 100 + 1) <= lossProb;
 }
 
-void printIP(uint32_t ip) {
+char* formatIP(uint32_t ip) {
     unsigned char bytes[4];
     bytes[0] = ip & 0xFF;
     bytes[1] = (ip >> 8) & 0xFF;
     bytes[2] = (ip >> 16) & 0xFF;
     bytes[3] = (ip >> 24) & 0xFF; 
-    printf("Pinger IP: %d.%d.%d.%d\n", bytes[0], bytes[1], bytes[2], bytes[3]);        
+    char* string = malloc(sizeof(char)*16);
+    sprintf(string, "%d.%d.%d.%d", bytes[0], bytes[1], bytes[2], bytes[3]);        
+    return string;
 }
 
 void printPacketInfo(QueuedPacket * queuedPacket) {
   printf("Packet Received\n");
   printf("Time: %lu\n", queuedPacket->timestamp);
-  printIP(queuedPacket->ipAddress);
+  printf("Pinger IP: %s\n", formatIP(queuedPacket->ipAddress));
   printf("Sequence #: %lu\n", (unsigned long)queuedPacket->packet->sequence);
 }
 
