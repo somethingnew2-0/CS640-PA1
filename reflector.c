2aee5f3c47c690d74e5d7d9d235d69d0c436cc01
Cory Groom <cpgroom@wisc.edu>
2013-10-19 14:52:04 -0500

    fixed printing ip

--- reflector.c
+++ reflector.c
@@ -11,19 +11,19 @@ bool dropPacket(int lossProb) {
   return (rand() % 100 + 1) <= lossProb;
 }
 
-char* formatIP(uint32_t ip) {
+void printIP(uint32_t ip) {
     unsigned char bytes[4];
     bytes[0] = ip & 0xFF;
     bytes[1] = (ip >> 8) & 0xFF;
     bytes[2] = (ip >> 16) & 0xFF;
     bytes[3] = (ip >> 24) & 0xFF; 
-    return sprintf("%d.%d.%d.%d", bytes[3], bytes[2], bytes[1], bytes[0]);        
+    printf("Pinger IP: %d.%d.%d.%d\n", bytes[0], bytes[1], bytes[2], bytes[3]);        
 }
 
 void printPacketInfo(QueuedPacket * queuedPacket) {
   printf("Packet Received\n");
   printf("Time: %lu\n", queuedPacket->timestamp);
-  printf("Pinger IP: %lu\n", (unsigned long)queuedPacket->ipAddress);
+  printIP(queuedPacket->ipAddress);
   printf("Sequence #: %lu\n", (unsigned long)queuedPacket->packet->sequence);
 }
 
